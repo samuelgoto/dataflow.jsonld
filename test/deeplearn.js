3bce552024044f89237f6af4c19a28515e41e418
samuelgoto@gmail.com <samuelgoto@gmail.com>
2018-08-21 17:35:57 -0700

    adds a test that loads a saved model from tf hub and executes a dataflow against an image

--- test/deeplearn.js
+++ test/deeplearn.js
@@ -1,12 +1,65 @@
+const fs = require("fs");
 const Assert = require("assert");
 
 const tf = require('@tensorflow/tfjs');
+const {loadFrozenModel} = require("@tensorflow/tfjs-converter");
+require("@tensorflow/tfjs-node");
+const image = require("get-image-data");
+const canvas = require("canvas");
 
-// Load the binding:
-require('@tensorflow/tfjs-node');  // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+const {IMAGENET_CLASSES} = require("./imagenet_classes.js")
+
+async function fetch(url) {
+ return new Promise(function(resolve, reject) {
+   image(url, function(error, info) {
+     if (error) {
+      reject(error);
+      return;
+     }
+     resolve(info);
+    });
+  });
+}
+
+async function load(url) {
+ return new Promise(function(resolve, reject) {
+   fs.readFile(url, (err, data) => {
+     // return;
+     // console.log("hi");
+     if (err) {
+      // console.log(err);
+      reject(err);
+      throw err;
+     }
+     // console.log(data);
+     const img = new canvas.Image()
+     // img.onload = () => ctx.drawImage(img, 0, 0)
+     img.onload = () => {
+      // console.log("hello");
+      // resolve(img);
+
+      let foo = new canvas(224, 224);
+      let context = foo.getContext("2d");
+      // context.drawImage(img, 0, 0, img.width / 4, img.height / 4);
+      context.drawImage(img, 0, 0, 224, 224);
+
+      resolve(foo);
+     }
+     img.onerror = (err) => { 
+      // console.log("hi");
+      console.log(err);
+      reject(err);
+      // throw err;
+     }
+     img.src = data;
+     // console.log(img);
+    });
+ });
+}
+
 
 describe("Deeplearn", function() {
-  it("Hello", async function() {
+  it.skip("training", async function() {
     // Train a simple model:
     const model = tf.sequential();
     model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
@@ -25,6 +78,129 @@ describe("Deeplearn", function() {
       }
      });
    });
+
+  it.skip("core concepts", async function() {
+    // 2x3 Tensor
+    const shape = [2, 3]; // 2 rows, 3 columns
+    tf.tensor([1.0, 2.0, 3.0, 10.0, 20.0, 30.0], shape).print();
+    // Output: [[1 , 2 , 3 ],
+    //          [10, 20, 30]]
+    
+    // The shape can also be inferred:
+    tf.tensor([[1.0, 2.0, 3.0], [10.0, 20.0, 30.0]]).print();
+    // Output: [[1 , 2 , 3 ],
+    //          [10, 20, 30]]
+    tf.zeros([3, 5]).print();
+
+    const initialValues = tf.zeros([5]);
+    const biases = tf.variable(initialValues); // initialize biases
+    biases.print(); // output: [0, 0, 0, 0, 0]
+
+    const updatedValues = tf.tensor1d([0, 1, 0, 1, 0]);
+    biases.assign(updatedValues); // update values of biases
+    biases.print(); // output: [0, 1, 0, 1, 0]
+
+    const d = tf.tensor2d([[1.0, 2.0], [3.0, 4.0]]);
+    d.square().print();
+
+    const e = tf.tensor2d([[1.0, 2.0], [3.0, 4.0]]);
+    const f = tf.tensor2d([[5.0, 6.0], [7.0, 8.0]]);
+
+    const e_plus_f = e.add(f);
+    e_plus_f.print();
+
+    const sq_sum = e.add(f).square();
+    sq_sum.print();
+
+
+    // Define function
+    function predict(input) {
+     // y = a * x ^ 2 + b * x + c
+     // More on tf.tidy in the next section
+     return tf.tidy(() => {
+       const x = tf.scalar(input);
+
+       const ax2 = a.mul(x.square());
+       const bx = b.mul(x);
+       const y = ax2.add(bx).add(c);
+
+       return y;
+      });
+    }
+
+    // Define constants: y = 2x^2 + 4x + 8
+    const a = tf.scalar(2);
+    const b = tf.scalar(4);
+    const c = tf.scalar(8);
+
+    // Predict output for input of 2
+    const result = predict(2);
+    result.print(); // Output: 24
+
+
+   });
+
+  it.only("tensorflow converter", async function() {
+
+    /// console.log(canvas);
+    // let canvas = new canvas.Canvas(200, 200);
+
+    // tensorflowjs_converter
+    //    --input_format=tf_hub
+    //    'https://tfhub.dev/google/imagenet/mobilenet_v1_100_224/classification/1'
+    //    mobilenet
+    const MODEL_URL = "file://./test/mobilenet/tensorflowjs_model.pb";
+    const WEIGHTS_URL = "file://./test/mobilenet/weights_manifest.json";
+    const model = await loadFrozenModel(MODEL_URL, WEIGHTS_URL);
+    // console.log(model);
+
+
+    let image = await load("./test/eiffel.jpg");
+
+    let input = tf.fromPixels(image);
+    const PREPROCESS_DIVISOR = tf.scalar(255 / 2);
+    
+    // console.log(image);
+    // console.log(tf.fromPixels(image).expandDims().shape);
+
+    const preprocessedInput = tf.div(tf.sub(input.asType('float32'), 
+                                            PREPROCESS_DIVISOR),PREPROCESS_DIVISOR);
+    const reshapedInput =
+     preprocessedInput.reshape([1, ...preprocessedInput.shape]);
+
+    // return;
+    let result = await model.execute({
+      // images: tf.fromPixels(image).expandDims().toFloat()
+      images: reshapedInput
+     });
+
+
+    function getTopKClasses(logits, topK) {
+     const predictions = tf.tidy(() => {
+       return tf.softmax(logits);
+      });
+
+     const values = predictions.dataSync();
+     predictions.dispose();
+
+     let predictionList = [];
+     for (let i = 0; i < values.length; i++) {
+      predictionList.push({value: values[i], index: i});
+     }
+     predictionList = predictionList
+      .sort((a, b) => {
+        return b.value - a.value;
+       })
+      .slice(0, topK);
+
+     return predictionList.map(x => {
+       return {label: IMAGENET_CLASSES[x.index], value: x.value};
+      });
+    }
+
+    console.log(getTopKClasses(result, 5));
+   });
+
 
   function assertThat(x) {
    return {
